afc2d1c91a4642621dc74d61fb9c9c2d967dc401
unknown <paikpawan18@gmail.com>
2024-07-16 00:23:50 +0530

    applied logic

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,23 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 import "./TaskForm.css";
 const TaskForm = () => {
+    
+    const [taskData,setTaskData]=useState({
+        task:"",
+        status:"todo",
+        description:""
+    })
+    const handleChange=(e)=>{
+        const {name,value}=e.target
+    //    const name=e.target.name;
+    //    const value=e.target.value;
+
+       setTaskData(prev=>{
+        return {...prev, [name]:value}
+       });
+       
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(taskData);
+    }
+    // const [task,setTask]=useState("");
+    // const [status,setStatus]=useState("todo");
+    // const [description,setDescription]=useState("");
+    
+    // const handletaskChange=e=>{
+    //     setTask(e.target.value);
+    // };
+    // const handleStatusChange=e=>{
+    //     setStatus(e.target.value);
+    // };
+    // const handleDescription=e=>{
+    //     setDescription(e.target.value);
+    // };
+    // console.log(task,status,description);
   return (
     <div>
-       <form className='form'>
+       <form className='form' onSubmit={handleSubmit}>
             <div className='box_form'>
                 <h1>Task</h1>
-                <level>Add Title</level>
-                <input placeholder='Add Title' type="text" /><br />
-                <level>Add Description</level>
-                <textarea placeholder='Add Description' type="text" /><br />
-                <div className='addtask'>
-                    <button type="submit" >Add</button>
+                <div className='formmain'>
+                    <div className='row'>
+                        <h1>Title</h1>
+                        <input placeholder='Add Title' type="text" name="task" onChange={handleChange}/>
+                    </div>
+                    <div className='row'>
+                        <h1>Description</h1>
+                        <textarea placeholder='Add Description' type="text" name="description" onChange={handleChange}/>
+                    </div>
+                    <div>
+                        <select name="status" className='task_status' onChange={handleChange}> 
+                            <option value='todo'>To Do</option>
+                            <option value='inprogress'>In Progress</option>
+                            <option value='peerreview'>Peer Review</option>
+                            <option value='done'>Done</option>
+                        </select>
+                    </div>
+                    <br />
                 </div>
                 
+                <div className='addtask'>
+                    <button type="submit">Add</button>
+                </div>
             </div>
-            
-
        </form>
         
     </div>
